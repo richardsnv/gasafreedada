b61c8092535fb6d48e7b2e232f3aff44bf7adb22
LawinFranck <lawin.dagan@gmail.com>
2025-02-01 20:13:42 +0100

    Create list.dart

---
+++ public/list.dart
@@ -0,0 +1,34 @@
+//créer une classe voiture 
+/*class Voiture{
+    String marque;
+    String model;
+
+    Voiure (this.marque, this.model);
+
+    // methode afficherDetailles
+
+
+    void afficherDetailles(){
+        print("marque: $marque, model: $model");
+    }
+
+    void main(){
+            v1 = voiture('Toyota', '4*4')
+    }
+    
+}
+*/
+
+//création d'une liste de trois nombres
+void main(){
+    void main() {
+  List<int> nombres = [10, 20, 30];
+  int somme = 0;
+  for (int i = 0; i < nombres.length; i++) {
+    somme += nombres[i];
+  }
+  print('Somme = $somme');
+}
+
+} 
+
